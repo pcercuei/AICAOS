4175a0815d0e25a9afa4664cf26e767571d99cab
Ayla <paul@crapouillou.net>
2011-12-01 19:04:55 +0100

    SH4: added sample main.c (for testing purposes).

---
+++ sh4/main.c
@@ -0,0 +1,44 @@
+
+#include <kos.h>
+
+#include "../aica_common.h"
+
+extern uint8 romdisk[];
+
+KOS_INIT_FLAGS(INIT_DEFAULT | INIT_MALLOCSTATS);
+
+KOS_INIT_ROMDISK(romdisk);
+
+/* Wrapper to the real function. */
+static SHARED(sh4_puts)
+{
+	return puts((char *)in);
+}
+
+static volatile int done = 0;
+
+static void * exit_thd(void *param)
+{
+	sleep(5);
+	done = 1;
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	thd_create(THD_DEFAULTS, exit_thd, NULL);
+
+	int ret = aica_init("/rd/aicaos.drv");
+	if (ret) {
+		printf("Error %i.\n", ret);
+		return ret;
+	}
+
+	AICA_SHARE(sh4_puts, 0x100, 0);
+	
+	printf("SH4: My job\'s complete.\n");
+
+	while(!done) thd_pass();
+	return 0;
+}
+
