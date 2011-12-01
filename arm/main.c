97991aa241d6bc9257bd2a699fac1271fd8b2a7c
Ayla <paul@crapouillou.net>
2011-12-01 19:04:14 +0100

    ARM: added sample main.c (for testing purposes).

---
+++ arm/main.c
@@ -0,0 +1,17 @@
+
+#include <stdint.h>
+
+#include "../aica_common.h"
+
+AICA_ADD_REMOTE(sh4_puts, 0);
+
+
+int main(int argc, char **argv)
+{
+	aica_init(NULL);
+
+	while (sh4_puts(NULL, "Hello World!") == -EAGAIN);
+
+	return 0;
+}
+
