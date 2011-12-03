0e8ff9d56b98cd2ac419092a90184871e6540467
Ayla <paul@crapouillou.net>
2011-12-03 13:11:22 +0100

    Use the standard linked-list macros for the handlers table.

--- aica_common.c
+++ aica_common.c
@@ -1,26 +1,28 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <sys/queue.h>
 
 #include "aica_common.h"
 
-static struct Handler {
+struct Handler {
 	aica_funcp_t handler;
 	const char *funcname;
 	unsigned int id;
-	struct Handler *next;
-} *handlers = NULL;
+	SLIST_ENTRY(Handler) next;
+};
 
+static SLIST_HEAD(Head, Handler) head = SLIST_HEAD_INITIALIZER(head);
 
 void __aica_share(aica_funcp_t func, const char *funcname, size_t sz_in, size_t sz_out)
 {
 	unsigned int id;
 	struct Handler *hdl = malloc(sizeof(struct Handler));
 
-	if (handlers == NULL)
-	  id = 0;
+	if (SLIST_FIRST(&head) == NULL)
+		id = 0;
 	else
-	  id = handlers->id +1;
+		id = SLIST_FIRST(&head)->id + 1;
 
 	struct function_params fparams = {
 		{ sz_in, sz_in ? malloc(sz_in) : NULL, },
@@ -33,23 +35,18 @@ void __aica_share(aica_funcp_t func, const char *funcname, size_t sz_in, size_t 
 	hdl->id = id;
 	hdl->handler = func;
 	hdl->funcname = funcname;
-	hdl->next = handlers;
 
-	handlers = hdl;
+	SLIST_INSERT_HEAD(&head, hdl, next);
 }
 
 
 int aica_clear_handler(unsigned int id)
 {
-	struct Handler *hdl, *prev;
+	struct Handler *hdl;
 
-	for (hdl = handlers, prev = NULL; hdl; prev = hdl, hdl = hdl->next) {
+	SLIST_FOREACH(hdl, &head, next) {
 		if (hdl->id == id) {
-			if (prev)
-			  prev->next = hdl->next;
-			else
-			  handlers = handlers->next;
-
+			SLIST_REMOVE(&head, hdl, Handler, next);
 			free(hdl);
 			return 0;
 		}
@@ -60,24 +57,27 @@ int aica_clear_handler(unsigned int id)
 
 void aica_clear_handler_table(void)
 {
-	struct Handler *next;
-	while (handlers) {
-		next = handlers->next;
-		free(handlers);
-		handlers = next;
+	struct Handler *hdl;
+
+	while(1) {
+		hdl = SLIST_FIRST(&head);
+		if (!hdl)
+			return;
+
+		SLIST_REMOVE_HEAD(&head, next);
+		free(hdl);
 	}
 }
 
 int aica_find_id(unsigned int *id, char *funcname)
 {
-	struct Handler *hdl = handlers;
+	struct Handler *hdl;
 
-	while(hdl) {
+	SLIST_FOREACH(hdl, &head, next) {
 		if (strcmp(hdl->funcname, funcname) == 0) {
 			*id = hdl->id;
 			return 0;
 		}
-		hdl = hdl->next;
 	}
 
 	return -1;
@@ -85,12 +85,11 @@ int aica_find_id(unsigned int *id, char *funcname)
 
 aica_funcp_t aica_get_func_from_id(unsigned int id)
 {
-	struct Handler *hdl = handlers;
+	struct Handler *hdl;
 
-	while(hdl) {
+	SLIST_FOREACH(hdl, &head, next) {
 		if (hdl->id == id)
-		  return hdl->handler;
-		hdl = hdl->next;
+			return hdl->handler;
 	}
 
 	return NULL;
@@ -98,12 +97,11 @@ aica_funcp_t aica_get_func_from_id(unsigned int id)
 
 const char * aica_get_funcname_from_id(unsigned int id)
 {
-	struct Handler *hdl = handlers;
+	struct Handler *hdl;
 
-	while(hdl) {
+	SLIST_FOREACH(hdl, &head, next) {
 		if (hdl->id == id)
 		  return hdl->funcname;
-		hdl = hdl->next;
 	}
 
 	return NULL;
