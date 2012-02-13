0d7e97a91eeeda89f075fc49f0024e6a7668c560
Paul Cercueil <paul@crapouillou.net>
2012-02-13 19:16:55 +0100

    ARM: define the reentrant stubs of newlib instead of the non-reentrant ones.
    All the internal functions ('write', 'printf', 'malloc' etc) of newlib call
    the reentrant functions; this is a necessary step to get malloc to cope with
    the RPC library.

--- arm/aica_syscalls.c
+++ arm/aica_syscalls.c
@@ -2,68 +2,64 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/times.h>
+#include <sys/reent.h>
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
-/* errno variable definition */
 #include <errno.h>
-#undef errno
-extern int errno;
 
-/* environment definition */
-char *__env[1] = { 0 };
-char **environ = __env;
-
-int execve(const char *name, char * const argv[], char * const env[])
+int _execve_r(struct _reent *r, const char *name, char * const argv[], char * const env[])
 {
-	errno = ENOMEM;
+	r->_errno = ENOMEM;
 	return -1;
 }
 
-int fork(void)
+int _fork_r(struct _reent *r)
 {
-	errno = EAGAIN;
+	r->_errno = EAGAIN;
 	return -1;
 }
 
-int getpid(void)
+int _getpid_r(struct _reent *r)
 {
 	return 1;
 }
 
-int kill(int pid, int sig)
+int _kill_r(struct _reent *r, int pid, int sig)
 {
-	errno = EINVAL;
+	r->_errno = EINVAL;
 	return -1;
 }
 
-clock_t times(struct tms *buf)
+clock_t _times_r(struct _reent *r, struct tms *buf)
 {
+	r->_errno = EACCES;
 	return -1;
 }
 
-int wait(int *status)
+int _wait_r(struct _reent *r, int *status)
 {
-	errno = ECHILD;
+	r->_errno = ECHILD;
 	return -1;
 }
 
 
-register char * stack_ptr asm ("sp");
+_READ_WRITE_RETURN_TYPE _write_r(struct _reent *, int, const void *, size_t);
 static char *heap_end = (char*) 0;
 
-void * sbrk(ptrdiff_t incr)
+void * _sbrk_r(struct _reent *r, ptrdiff_t incr)
 {
 	extern char _end;		/* Defined by the linker */
 	char *prev_heap_end;
+	register char * stack_ptr asm ("sp");
 
 	if (heap_end == 0) {
 		heap_end = &_end;
 	}
 	prev_heap_end = heap_end;
 	if (heap_end + incr > stack_ptr) {
-		write (1, "Heap and stack collision\n", 25);
+		_write_r (r, 1, "Heap and stack collision\n", 25);
 		abort();
 	}
 
@@ -87,115 +83,115 @@ AICA_ADD_REMOTE(sh4_lseek, PRIORITY_DEFAULT);
 AICA_ADD_REMOTE(sh4_read, PRIORITY_DEFAULT);
 AICA_ADD_REMOTE(sh4_write, PRIORITY_DEFAULT);
 
-int open(const char *name, int flags, int mode)
+int _open_r(struct _reent *r, const char *name, int flags, int mode)
 {
 	int result;
 	struct open_param params = { name, strlen(name), flags, mode, };
 
 	if ( sh4_open(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-int close(int file)
+int _close_r(struct _reent *r, int file)
 {
 	int result;
 
 	if ( sh4_close(&result, &file) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-int fstat(int file, struct stat *st)
+int _fstat_r(struct _reent *r, int file, struct stat *st)
 {
 	int result;
 	struct fstat_param params = { file, st, };
 
 	if ( sh4_fstat(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-int stat(const char *file, struct stat *st)
+int _stat_r(struct _reent *r, const char *file, struct stat *st)
 {
 	int result;
 	struct stat_param params = { file, strlen(file), st, };
 
 	if ( sh4_stat(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-int isatty(int file)
+int _isatty_r(struct _reent *r, int file)
 {
 	int result;
 
 	if ( sh4_isatty(&result, &file) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return 0;
 	}
 
 	return result;
 }
 
-int link(const char *old, const char *new)
+int _link_r(struct _reent *r, const char *old, const char *new)
 {
 	int result;
 	struct link_param params = { old, strlen(old), new, strlen(new), };
 
 	if ( sh4_link(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-off_t lseek(int file, off_t ptr, int dir)
+off_t _lseek_r(struct _reent *r, int file, off_t ptr, int dir)
 {
 	int result;
 	struct lseek_param params = { file, ptr, dir, };
 
 	if ( sh4_lseek(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-_READ_WRITE_RETURN_TYPE read(int file, void *ptr, size_t len)
+_READ_WRITE_RETURN_TYPE _read_r(struct _reent *r, int file, void *ptr, size_t len)
 {
 	int result;
 	struct read_param params = { file, ptr, len, };
 
 	if ( sh4_read(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
 	return result;
 }
 
-_READ_WRITE_RETURN_TYPE write(int file, const void *ptr, size_t len)
+_READ_WRITE_RETURN_TYPE _write_r(struct _reent *r, int file, const void *ptr, size_t len)
 {
 	int result;
 	struct write_param params = { file, ptr, len, };
 
 	if ( sh4_write(&result, &params) != 0 ) {
-		errno = EAICA;
+		r->_errno = EAICA;
 		return -1;
 	}
 
