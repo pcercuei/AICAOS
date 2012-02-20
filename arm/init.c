1252400e28a4aaa7b9cb2ff714c4c1ef5c5918a9
Paul Cercueil <paul@crapouillou.net>
2012-02-20 13:34:55 +0100

    ARM: use 3 handlers for each of the 3 FIQs instead of a common one.

--- arm/init.c
+++ arm/init.c
@@ -31,27 +31,32 @@ uint32_t int_enable(void)
 	return cpsr;
 }
 
-void aica_sh4_fiq_hdl(void)
+
+static void acknowledge(void)
 {
-}
-
-void __attribute__((interrupt ("FIQ"))) fiq_hdl(void)
-{
-	uint32_t i;
-
-	switch (*(unsigned int *) REG_ARM_FIQ_CODE & 0x7) {
-		case TIMER_INTERRUPT_INT_CODE:
-			break;
-		case BUS_REQUEST_INT_CODE:
-			while(0x100 & *(volatile unsigned int *) REG_BUS_REQUEST);
-			break;
-		case SH4_INTERRUPT_INT_CODE:
-			aica_sh4_fiq_hdl();
-			break;
-	}
-
-	/* Acknowledge */
-	for (i=0; i<4; i++)
+	unsigned int i;
+	for (i = 0; i < 4; i++)
 		*(unsigned int *) REG_ARM_FIQ_ACK = 1;
 }
 
+
+/* Called from crt0.S */
+void __attribute__((interrupt ("FIQ"))) sh4_fiq_hdl(void)
+{
+	acknowledge();
+}
+
+
+/* Called from crt0.S */
+void __attribute__((interrupt ("FIQ"))) bus_fiq_hdl(void)
+{
+	while(0x100 & *(volatile unsigned int *) REG_BUS_REQUEST);
+	acknowledge();
+}
+
+
+/* Called from crt0.S */
+void __attribute__((interrupt ("FIQ"))) timer_fiq_hdl(void)
+{
+	acknowledge();
+}
